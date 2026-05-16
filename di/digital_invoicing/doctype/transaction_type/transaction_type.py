0807a33b259b845606fe928441aa273d01a4d5c5
Ali Raza <soundsofnation29@gmail.com>
2026-05-16 17:22:26 +0500

    feat: add auto-generated type hints, boilerplate files, and doctype configuration updates

--- di/digital_invoicing/doctype/transaction_type/transaction_type.py
+++ di/digital_invoicing/doctype/transaction_type/transaction_type.py
@@ -3,4 +3,15 @@ from frappe.model.document import Document
 
 
 class TransactionType(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		transaction_desc: DF.Data
+		transaction_type_id: DF.Int
+	# end: auto-generated types
 	pass
